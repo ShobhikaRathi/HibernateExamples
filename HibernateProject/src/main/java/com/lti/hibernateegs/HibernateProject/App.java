b4a8fcbb4c881745c76b4ee123b92e9209f0b6c4
Shobhikarathi <shobhika18@gmail.comgit config --global user.name Shobhikarathi>
2019-11-06 16:25:26 +0530

    committed

--- HibernateProject/src/main/java/com/lti/hibernateegs/HibernateProject/App.java
+++ HibernateProject/src/main/java/com/lti/hibernateegs/HibernateProject/App.java
@@ -1,13 +1,23 @@
 package com.lti.hibernateegs.HibernateProject;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String args[] )
     {
-        System.out.println( "Hello World!" );
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = new  StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+        SessionFactory factory = configuration.buildSessionFactory(builder.build());
+        Session session = factory.openSession();
+        Employeees employee = new Employeees();
+        employee.setEmpName("shobhika");
+        employee.setBranch("Chennai");
+        session.beginTransaction();
+        session.save(employee);
+        session.getTransaction().commit();
     }
 }
